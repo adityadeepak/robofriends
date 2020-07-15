2cee27ef707ab8c2c9ebec624477005b00c3adb0
adityadeepak <33161829+adityadeepak@users.noreply.github.com>
2020-07-15 13:34:45 +0530

    Updates

---
+++ precache-manifest.f85efa55cbcfb6fcde380e1ac0b59840.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca39b323a76424d39f4233c474d3f8d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8e83eb3284956c407a60",
+    "url": "/robofriends/static/css/main.e98e81c3.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e83eb3284956c407a60",
+    "url": "/robofriends/static/js/main.d015edf6.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "9cb50352b3b1b486045db78f3eaad2df",
+    "url": "/robofriends/static/media/Quickat-vLKD.9cb50352.woff"
+  }
+]);
